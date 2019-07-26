f333a0730fa3450db449acb5a7446bb1bb8697a7
sunyujian <sunyujian@souche.com>
2019-07-26 23:40:00 +0800

    feat: menulist

--- src/layouts/MenuList.js
+++ src/layouts/MenuList.js
@@ -1,61 +1,131 @@
 import React, { Component } from 'react'
-import { Text, View, SafeAreaView, StyleSheet, StatusBar, FlatList, TouchableOpacity } from 'react-native'
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+  RefreshControl
+} from "react-native";
 import { getMenuList } from "../requests";
+import { px } from '../utils'
+import { Header } from '../components';
 
 export class MenuList extends Component {
-
     constructor(props) {
         super(props);
-        const { articleid } = props.navigation.state.params || {}
+        const { articleid } = props.navigation.state.params || {};
         this.state = {
           pageIndex: 1,
           articleid: articleid,
           totalPage: null,
-          menuList: []
-        }
+          menuList: [],
+          isRefresh: false
+        };
     }
 
     componentDidMount() {
-        this.getMenuListAction()
+    this.getMenuListAction();
     }
 
     render() {
-        
         const { menuList } = this.state;
         console.log("menuList", menuList);
         return (
-            <View style={styles.container}>
-                <StatusBar barStyle="dark-content" />
-                <SafeAreaView style={styles.safeView}>
-                <FlatList
-                    data={menuList}
-                    renderItem={this.renderItem.bind(this)}
-                />
-                </SafeAreaView>
-            </View>
-        )
+          <View style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView style={styles.safeView}>
+              <Header title={""} showBackButton={true} navigation={this.props.navigation} />
+              <Text style={styles.menuText}>目录</Text>
+              <FlatList
+                data={menuList}
+                renderItem={this.renderItem.bind(this)}
+                refreshControl={
+                  <RefreshControl
+                    title={"Loading"}
+                    colors={["red"]}
+                    refreshing={this.state.isRefresh}
+                    onRefresh={() => {
+                      this._onRefresh();
+                    }}
+                  />
+                }
+                onEndReached={this.loadMore.bind(this)}
+                onEndReachedThreshold={0.1}
+                refreshing={this.state.isRefresh}
+                ListFooterComponent={() => {
+                  return (
+                    <View
+                      style={{
+                        height: px(60),
+                        alignItems: "center",
+                        justifyContent: "center"
+                      }}
+                    >
+                      <Text style={{ color: "#999" }}>没有更多了~</Text>
+                    </View>
+                  );
+                }}
+              />
+            </SafeAreaView>
+          </View>
+        );
     }
 
     // render
     renderItem = ({ item }) => {
         return (
-          <TouchableOpacity>
-            <Text>{item.chaptername}</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.itemContent}>
+            <Text style={styles.chapterName}>{item.chaptername}</Text>
+            </TouchableOpacity>
         );
-    }
+    };
 
     // method
 
     // request
     getMenuListAction() {
         const data = {
+            pageSize: 10,
+            pageIndex: this.state.pageIndex,
+            callback: this.getMenuListCallback.bind(this),
+            articleid: this.state.articleid
+        };
+        getMenuList(data);
+    }
+
+    _onRefresh() {
+
+        this.setState({
+            menuList: []
+        })
+
+        const data = {
+            pageSize: 10,
+            pageIndex: 1,
+            callback: this.getMenuListCallback.bind(this),
+            articleid: this.state.articleid
+        }
+        getMenuList(data);
+
+        setTimeout(() => {
+            this.setState({
+              isRefresh: false
+            });
+        }, 6000)
+
+    }
+
+    loadMore() {
+        const data = {
           pageSize: 10,
-          pageIndex: this.state.pageIndex,
+          pageIndex: this.state.pageIndex + 1,
           callback: this.getMenuListCallback.bind(this),
           articleid: this.state.articleid
         };
-        getMenuList(data)
+        getMenuList(data);
     }
 
     getMenuListCallback(res) {
@@ -64,13 +134,13 @@ export class MenuList extends Component {
         if (state == 1) {
             const { totalPage, pageIndex } = page;
             this.setState({
-                totalPage: totalPage,
-                pageIndex: pageIndex + 1,
-                menuList: this.state.menuList.concat(data)
-            })
+              totalPage: totalPage,
+              pageIndex: pageIndex,
+              menuList: this.state.menuList.concat(data),
+              isRefresh: false
+            });
         }
     }
-
 }
 
 const styles = StyleSheet.create({
@@ -85,6 +155,23 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     backgroundColor: "#F6F7FB"
+  },
+  menuText: {
+    marginLeft: px(120),
+    fontSize: px(38),
+    color: "#656E79",
+    marginTop: px(30),
+    marginBottom: px(30)
+  },
+  itemContent: {
+    height: px(100),
+    //   alignItems: "center"
+    justifyContent: "center",
+    paddingLeft: px(120)
+  },
+  chapterName: {
+    color: "#656E79",
+    fontSize: px(30)
   }
 });
 
